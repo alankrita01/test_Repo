7618af1f530f6684fa2742238788cc21462611e1
Alankrita Patidar <alankritapatidar@gmail.com>
2023-06-24 11:37:15 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const express = require('express');
+const sequelize = require('./util/database');
+
+const userRoute = require('./routes/userRoute');
+const expenseRoute = require('./routes/expenseRoute');
+
+const User = require('./models/userModel');
+const Expense = require('./models/expenseModel');
+
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+app.use(cors());
+app.use(express.json());
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', async (req,res,next) => {
+    res.sendFile(__dirname+'/public/signup.html')
+})
+
+app.use(express.static('public'));
+
+
+
+app.use('/user',userRoute);
+app.use('/expense',expenseRoute);
+
+sequelize.sync({alter : true})
+.then(result => {
+  app.listen(3000, () => {
+    console.log('server started');
+  })
+})
+.catch(err => console.log(err));
