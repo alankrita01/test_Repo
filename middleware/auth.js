8a969bbdd31a20582b9a93f919b49c64114b20e3
Alankrita Patidar <alankritapatidar@gmail.com>
2023-07-19 20:04:38 +0530

    adding Auth.js

---
+++ middleware/auth.js
@@ -0,0 +1,31 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/userModel');
+
+const authenticate = (req, res, next) => {
+
+    try{
+        const token = req.header('Authorization');
+        console.log(token);
+        const user = jwt.verify(token, 'secretKey');
+        console.log('userId >>>', user.userId);
+
+        User.findByPk(userId).then(user => {
+            //console.log(JSON.stringify(user));
+            req.user = user;
+            next();
+        })
+        .catch(err => {
+            throw new Error(err)
+        })
+
+    }
+    catch(err){
+        console.log(err);
+        return res.status(401).json({success: false})
+    }
+    
+}
+
+module.exports ={
+    authenticate
+}
