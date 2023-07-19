8f38394ead3c431f1ce6e72709d14954de919a1d
Alankrita Patidar <alankritapatidar@gmail.com>
2023-07-19 20:06:09 +0530

    adding expenseRoute.js

--- routes/expenseRoute.js
+++ routes/expenseRoute.js
@@ -3,13 +3,13 @@ const express = require("express");
 const router = express.Router();
 
 const expenseController = require('../controller/expenses');
+const userAuthentication = require('../middleware/auth');
 
 router.post('/add-expense',expenseController.addExpense);
 
-router.get('/get-expense',expenseController.getExpense);
+router.get('/get-expense',userAuthentication.authenticate, expenseController.getExpense);
 
-router.delete('/delete-expense',expenseController.deleteExpense);
+router.delete('/delete-expense/:expenseId',expenseController.deleteExpense);
 
-router.put('/edit-expense',expenseController.getExpense);
 
 module.exports = router;
